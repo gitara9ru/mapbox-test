3519426983ca1761381cfe4dbeaa0c48c8dd490d
gitara9ru <gitara9ru0611@gmail.com>
2021-09-24 23:11:24 +0900

    vuetify追加

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import doetnv from "dotenv";
+
 // @ts-ignore
 import css from "./assets/css/main.css";
+
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 doetnv.config();
@@ -13,5 +16,6 @@ new Vue({
   router,
   store,
   css,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
